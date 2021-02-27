f8672d420c940e667f274bcb084b7e93db7531cd
unknown <adiguna.hakim@gmail.com>
2021-02-28 06:38:25 +0700

    add if condition & add intelij file gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
 fn main() {
-    let x = 6;
-    let y = {
-        let x = 3;
-        x
-    };
+    let number = 3;
 
-    another_function(x, y);
-    println!("The value of function five() is {}", five());
-    println!("The value of function plus_one() is {}", plus_one(five()));
-}
-
-fn another_function(x: i32, y: i32) {
-    println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
-}
-
-fn five() -> i32 {
-    5
-}
-
-fn plus_one(x: i32) -> i32  {
-    x + 1
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
 }
