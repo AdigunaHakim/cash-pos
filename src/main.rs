0d05a8f3804fac4c2b1ab2f7d4bfa9ea0a8e11de
unknown <adiguna.hakim@gmail.com>
2021-02-28 08:21:30 +0700

    assign value with if expresion

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 fn main() {
-    let number = 3;
+    let number = 11;
 
-    if number < 5 {
-        println!("condition was true");
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
     } else {
-        println!("condition was false");
+        println!("number is not divisible by 4, 3, or 2");
     }
+    println!("The value of func statement_if is: {}", statement_if());
+}
+
+//function  with return value
+fn statement_if() -> i32 {
+    let condition = true;
+
+    //assign with if expresion
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {}", number);
+    number
 }
